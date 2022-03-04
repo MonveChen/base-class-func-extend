7744a8dfc9f193d36c0f45eac2d55fccdc7f92a8
Monve <chenmenghui@lazbao.com>
2022-03-04 14:43:35 +0800

    feat: Array.prototype.includeArr

--- src/array.ts
+++ src/array.ts
@@ -2,13 +2,14 @@
  * @Author: Monve
  * @Date: 2022-02-28 15:41:10
  * @LastEditors: Monve
- * @LastEditTime: 2022-02-28 16:00:07
+ * @LastEditTime: 2022-03-04 14:37:31
  * @FilePath: /base-class-fuction-extend/src/array.ts
  */
 
 declare interface Array<T> {
   remove(...item: T[]): void
   deduplicatePush(...item: T[]): void
+  includeArr(arr: T[]): boolean
 }
 
 Array.prototype.remove = function <T>(...item: T[]): void {
@@ -23,4 +24,8 @@ Array.prototype.remove = function <T>(...item: T[]): void {
 Array.prototype.deduplicatePush = function <T>(...item: T[]): void {
   this.remove(item)
   this.push(item)
+}
+
+Array.prototype.includeArr = function <T>(arr: T[]): boolean {
+  return this.every(val => arr.includes(val))
 }
