cc7fa286c9ac90194a0e2a3271d43ab66ea80100
Monve <chenmenghui@lazbao.com>
2022-05-09 14:01:07 +0800

    feat: deduplicateConcat

--- src/array.ts
+++ src/array.ts
@@ -2,18 +2,19 @@
  * @Author: Monve
  * @Date: 2022-02-28 15:41:10
  * @LastEditors: Monve
- * @LastEditTime: 2022-03-23 10:18:33
+ * @LastEditTime: 2022-05-09 13:59:10
  * @FilePath: /base-class-func-extend/src/array.ts
  */
 
 declare interface Array<T> {
   remove(...item: T[]): void
   deduplicatePush(...item: T[]): void
+  deduplicateConcat(...items: T[][]): T[]
   includeArr(arr: T[]): boolean
 }
 
 Array.prototype.remove = function <T>(...item: T[]): void {
-  item.forEach((obj: T) => {
+  item.forEach((obj) => {
     const idx = this.indexOf(obj)
     if (idx != -1) {
       this.splice(idx, idx + 1)
@@ -24,6 +25,13 @@ Array.prototype.remove = function <T>(...item: T[]): void {
 Array.prototype.deduplicatePush = function <T>(...item: T[]): void {
   this.remove(item)
   this.push(item)
+}
+
+Array.prototype.deduplicateConcat = function <T>(...items: T[][]): T[] {
+  items.forEach((item) => {
+    this.deduplicatePush(...item)
+  })
+  return this
 }
 
 Array.prototype.includeArr = function <T>(arr: T[]): boolean {
