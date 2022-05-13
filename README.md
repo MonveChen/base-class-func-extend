79fad2297e74f19ad3e273a8216ef95cc4694954
Monve <chenmenghui@lazbao.com>
2022-05-13 14:49:22 +0800

    feat:add Array.prototype.isSameTo

--- README.md
+++ README.md
@@ -7,10 +7,19 @@ A base class commonly used function extension plugin
 ```js
 import "base-class-func-extend" //import it first
 
-const a = ['a']
+const a = ['a','b']
+
 a.remove('a')
-a.remove('a','b')
-a.deduplicatePush('a')
+
+a.remove('a','b')	//移除 a、b   []
+
+a.deduplicatePush('a')  //去重式push.  ['a','a']
+
+a.includeArr(['b','c'])    //是否包含数组.  false
+
+a.isSameTo(['b','a'])    //是否相同(与排序无关).  true
+
+Object.reverseKeyValue({'a':1})   //key、value反转   {1:'a'}
 
 #javascript
 require("base-class-func-extend")
--- src/array.ts
+++ src/array.ts
@@ -2,7 +2,7 @@
  * @Author: Monve
  * @Date: 2022-02-28 15:41:10
  * @LastEditors: Monve
- * @LastEditTime: 2022-05-09 15:46:23
+ * @LastEditTime: 2022-05-13 14:29:20
  * @FilePath: /base-class-func-extend/src/array.ts
  */
 
@@ -11,6 +11,7 @@ declare interface Array<T> {
   deduplicatePush(...item: T[]): void
   deduplicateConcat(...items: T[][]): T[]
   includeArr(...items: T[][]): boolean
+  isSameTo(...items: T[][]): boolean
 }
 
 Array.prototype.remove = function <T>(...item: T[]): void {
@@ -37,5 +38,11 @@ Array.prototype.deduplicateConcat = function <T>(...items: T[][]): T[] {
 Array.prototype.includeArr = function <T>(...items: T[][]): boolean {
   return items.every(item =>
     item.every(val => this.includes(val))
+  )
+}
+
+Array.prototype.isSameTo = function <T>(...items: T[][]): boolean {
+  return items.every(item =>
+    this.includeArr(item) && item.includeArr(this)
   )
 }
