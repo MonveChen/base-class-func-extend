380c243bd80b9318a1d1887e1ec11937bd131be9
Monve <menghuiyouxiang@gmail.com>
2022-12-13 14:10:26 +0800

    feat: add Object.removeKeyWithValue

--- src/object.ts
+++ src/object.ts
@@ -2,7 +2,7 @@
  * @Author: Monve
  * @Date: 2022-03-10 14:42:14
  * @LastEditors: Monve
- * @LastEditTime: 2022-03-10 15:28:15
+ * @LastEditTime: 2022-12-13 14:09:18
  * @FilePath: /base-class-func-extend/src/object.ts
  */
 
@@ -21,4 +21,21 @@ Object.reverseKeyValue = function (o: Object): any {
     temp[(o as any)[key]] = key
   })
   return temp
+}
+
+declare interface ObjectConstructor {
+  removeKeyWithValue(o: Object, ...values: any[]): void
+}
+
+/**
+ * remove key with specified value
+ * like {a:b}->{b:a},other can not be ensure availability.
+ * Use with caution
+ */
+Object.removeKeyWithValue = function (targe: Object, ...values: any[]): void {
+  Object.keys(targe).map((key) => {
+    if (values.includes((targe as any)[key])) {
+      delete (targe as any)[key]
+    }
+  })
 }
